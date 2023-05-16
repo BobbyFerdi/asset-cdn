9e953631e4dcc22add269eaf2ea11402ecf50056
Bobby Ferdi <vertlink@gmail.com>
2023-05-16 20:27:57 +0700

    Updated cafe.html

--- cafe_files/cafe.js
+++ cafe_files/cafe.js
@@ -1,6 +1,6 @@
-(function($) {
-  $.fn.redraw = function() {
-    return this.map(function(){ this.offsetTop; return this; });
+(function ($) {
+  $.fn.redraw = function () {
+    return this.map(function () { this.offsetTop; return this; });
   };
 })(jQuery);
 
@@ -9,7 +9,7 @@ var Cafe = {
   modeOrder: false,
   totalPrice: 0,
 
-  init: function(options) {
+  init: function (options) {
     Telegram.WebApp.ready();
     Cafe.apiUrl = options.apiUrl;
     Cafe.mode = options.mode;
@@ -17,21 +17,21 @@ var Cafe = {
     Cafe.userHash = options.userHash;
     Cafe.initLotties();
     $('body').show();
-    if ((!Telegram.WebApp.initDataUnsafe ||
-         !Telegram.WebApp.initDataUnsafe.query_id) &&
-        Cafe.mode != 'inline' &&
-        Cafe.mode != 'link') {
-      Cafe.isClosed = true;
-      $('body').addClass('closed');
-      Cafe.showStatus('Cafe is temporarily closed');
-      return;
-    }
+    // if ((!Telegram.WebApp.initDataUnsafe ||
+    //      !Telegram.WebApp.initDataUnsafe.query_id) &&
+    //     Cafe.mode != 'inline' &&
+    //     Cafe.mode != 'link') {
+    //   Cafe.isClosed = true;
+    //   $('body').addClass('closed');
+    //   Cafe.showStatus('Cafe is temporarily closed');
+    //   return;
+    // }
     $('.js-item-lottie').on('click', Cafe.eLottieClicked);
     $('.js-item-incr-btn').on('click', Cafe.eIncrClicked);
     $('.js-item-decr-btn').on('click', Cafe.eDecrClicked);
     $('.js-order-edit').on('click', Cafe.eEditClicked);
     $('.js-status').on('click', Cafe.eStatusClicked);
-    $('.js-order-comment-field').each(function() {
+    $('.js-order-comment-field').each(function () {
       autosize(this);
     });
     Telegram.WebApp.MainButton.setParams({
@@ -41,8 +41,8 @@ var Cafe = {
     Telegram.WebApp.setHeaderColor('bg_color');
     initRipple();
   },
-  initLotties: function() {
-    $('.js-item-lottie').each(function() {
+  initLotties: function () {
+    $('.js-item-lottie').each(function () {
       RLottie.init(this, {
         maxDeviceRatio: 2,
         cachingModulo: 3,
@@ -50,45 +50,45 @@ var Cafe = {
       });
     });
   },
-  eLottieClicked: function(e) {
+  eLottieClicked: function (e) {
     if (Cafe.isClosed) {
       return false;
     }
     RLottie.playOnce(this);
   },
-  eIncrClicked: function(e) {
+  eIncrClicked: function (e) {
     e.preventDefault();
     Telegram.WebApp.HapticFeedback.impactOccurred('light');
     var itemEl = $(this).parents('.js-item');
     Cafe.incrClicked(itemEl, 1);
   },
-  eDecrClicked: function(e) {
+  eDecrClicked: function (e) {
     e.preventDefault();
     Telegram.WebApp.HapticFeedback.impactOccurred('light');
     var itemEl = $(this).parents('.js-item');
     Cafe.incrClicked(itemEl, -1);
   },
-  eEditClicked: function(e) {
+  eEditClicked: function (e) {
     e.preventDefault();
     Cafe.toggleMode(false);
   },
-  backBtnClicked: function() {
+  backBtnClicked: function () {
     Cafe.toggleMode(false);
   },
-  getOrderItem: function(itemEl) {
+  getOrderItem: function (itemEl) {
     var id = itemEl.data('item-id');
-    return $('.js-order-item').filter(function() {
+    return $('.js-order-item').filter(function () {
       return ($(this).data('item-id') == id);
     });
   },
-  updateItem: function(itemEl, delta) {
+  updateItem: function (itemEl, delta) {
     var price = +itemEl.data('item-price');
     var count = +itemEl.data('item-count') || 0;
     var counterEl = $('.js-item-counter', itemEl);
     counterEl.text(count ? count : 1);
     var isSelected = itemEl.hasClass('selected');
     if (!isSelected && count > 0) {
-      $('.js-item-lottie', itemEl).each(function() {
+      $('.js-item-lottie', itemEl).each(function () {
         RLottie.playOnce(this);
       });
     }
@@ -110,7 +110,7 @@ var Cafe = {
 
     Cafe.updateTotalPrice();
   },
-  incrClicked: function(itemEl, delta) {
+  incrClicked: function (itemEl, delta) {
     if (Cafe.isLoading || Cafe.isClosed) {
       return false;
     }
@@ -122,10 +122,10 @@ var Cafe = {
     itemEl.data('item-count', count);
     Cafe.updateItem(itemEl, delta);
   },
-  formatPrice: function(price) {
+  formatPrice: function (price) {
     return '$' + Cafe.formatNumber(price / 1000, 2, '.', ',');
   },
-  formatNumber: function(number, decimals, decPoint, thousandsSep) {
+  formatNumber: function (number, decimals, decPoint, thousandsSep) {
     number = (number + '').replace(/[^0-9+\-Ee.]/g, '')
     var n = !isFinite(+number) ? 0 : +number
     var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
@@ -154,12 +154,12 @@ var Cafe = {
     }
     return s.join(dec)
   },
-  updateBackgroundColor: function() {
+  updateBackgroundColor: function () {
     var style = window.getComputedStyle(document.body);
     var bg_color = parseColorToHex(style.backgroundColor || '#fff');
     Telegram.WebApp.setBackgroundColor(bg_color);
   },
-  updateMainButton: function() {
+  updateMainButton: function () {
     var mainButton = Telegram.WebApp.MainButton;
     if (Cafe.modeOrder) {
       if (Cafe.isLoading) {
@@ -183,9 +183,9 @@ var Cafe = {
     }
     Telegram.WebApp.isClosingConfirmationEnabled = !!Cafe.canPay;
   },
-  updateTotalPrice: function() {
+  updateTotalPrice: function () {
     var total_price = 0;
-    $('.js-item').each(function() {
+    $('.js-item').each(function () {
       var itemEl = $(this)
       var price = +itemEl.data('item-price');
       var count = +itemEl.data('item-count') || 0;
@@ -195,19 +195,19 @@ var Cafe = {
     Cafe.totalPrice = total_price;
     Cafe.updateMainButton();
   },
-  getOrderData: function() {
+  getOrderData: function () {
     var order_data = [];
-    $('.js-item').each(function() {
+    $('.js-item').each(function () {
       var itemEl = $(this)
-      var id    = itemEl.data('item-id');
+      var id = itemEl.data('item-id');
       var count = +itemEl.data('item-count') || 0;
       if (count > 0) {
-        order_data.push({id: id, count: count});
+        order_data.push({ id: id, count: count });
       }
     });
     return JSON.stringify(order_data);
   },
-  toggleMode: function(mode_order) {
+  toggleMode: function (mode_order) {
     Cafe.modeOrder = mode_order;
     var anim_duration, match;
     try {
@@ -225,26 +225,26 @@ var Cafe = {
     }
     if (mode_order) {
       var height = $('.cafe-items').height();
-      $('.js-item-lottie').each(function() {
+      $('.js-item-lottie').each(function () {
         RLottie.pause(this);
       });
       $('.cafe-order-overview').show();
       $('.cafe-items').css('maxHeight', height).redraw();
       $('body').addClass('order-mode');
-      $('.js-order-comment-field').each(function() {
+      $('.js-order-comment-field').each(function () {
         autosize.update(this);
       });
       Telegram.WebApp.expand();
       Telegram.WebApp.BackButton.show();
     } else {
-      $('.js-item-lottie').each(function() {
+      $('.js-item-lottie').each(function () {
         RLottie.reset(this);
       });
       $('body').removeClass('order-mode');
-      setTimeout(function() {
+      setTimeout(function () {
         $('.cafe-items').css('maxHeight', '');
         $('.cafe-order-overview').hide();
-        $('.js-item-lottie').each(function() {
+        $('.js-item-lottie').each(function () {
         });
       }, anim_duration);
       Telegram.WebApp.BackButton.hide();
@@ -252,13 +252,13 @@ var Cafe = {
     Cafe.updateBackgroundColor();
     Cafe.updateMainButton();
   },
-  toggleLoading: function(loading) {
+  toggleLoading: function (loading) {
     Cafe.isLoading = loading;
     Cafe.updateMainButton();
     $('body').toggleClass('loading', !!Cafe.isLoading);
     Cafe.updateTotalPrice();
   },
-  mainBtnClicked: function() {
+  mainBtnClicked: function () {
     if (!Cafe.canPay || Cafe.isLoading || Cafe.isClosed) {
       return false;
     }
@@ -280,7 +280,7 @@ var Cafe = {
         params.invoice = 1;
       }
       Cafe.toggleLoading(true);
-      Cafe.apiRequest('makeOrder', params, function(result) {
+      Cafe.apiRequest('makeOrder', params, function (result) {
         Cafe.toggleLoading(false);
         if (result.ok) {
           if (Cafe.mode == 'inline') {
@@ -288,7 +288,7 @@ var Cafe = {
           } else if (Cafe.mode == 'link') {
             Telegram.WebApp.switchInlineQuery('#' + result.order_id, ['users', 'groups']);
           } else if (invoiceSupported) {
-            Telegram.WebApp.openInvoice(result.invoice_url, function(status) {
+            Telegram.WebApp.openInvoice(result.invoice_url, function (status) {
               if (status == 'paid') {
                 Telegram.WebApp.close();
               } else if (status == 'failed') {
@@ -312,34 +312,34 @@ var Cafe = {
       Cafe.toggleMode(true);
     }
   },
-  eStatusClicked: function() {
+  eStatusClicked: function () {
     Cafe.hideStatus();
   },
-  showStatus: function(text) {
+  showStatus: function (text) {
     clearTimeout(Cafe.statusTo);
     $('.js-status').text(text).addClass('shown');
     if (!Cafe.isClosed) {
-      Cafe.statusTo = setTimeout(function(){ Cafe.hideStatus(); }, 2500);
+      Cafe.statusTo = setTimeout(function () { Cafe.hideStatus(); }, 2500);
     }
   },
-  hideStatus: function() {
+  hideStatus: function () {
     clearTimeout(Cafe.statusTo);
     $('.js-status').removeClass('shown');
   },
-  apiRequest: function(method, data, onCallback) {
+  apiRequest: function (method, data, onCallback) {
     var authData = Telegram.WebApp.initData || '';
     $.ajax(Cafe.apiUrl, {
       type: 'POST',
-      data: $.extend(data, {_auth: authData, method: method}),
+      data: $.extend(data, { _auth: authData, method: method }),
       dataType: 'json',
       xhrFields: {
         withCredentials: true
       },
-      success: function(result) {
+      success: function (result) {
         onCallback && onCallback(result);
       },
-      error: function(xhr) {
-        onCallback && onCallback({error: 'Server error'});
+      error: function (xhr) {
+        onCallback && onCallback({ error: 'Server error' });
       }
     });
   }
@@ -369,17 +369,17 @@ function parseColorToHex(color) {
   license: MIT
   http://www.jacklmoore.com/autosize
 */
-!function(e,t){if("function"==typeof define&&define.amd)define(["exports","module"],t);else if("undefined"!=typeof exports&&"undefined"!=typeof module)t(exports,module);else{var n={exports:{}};t(n.exports,n),e.autosize=n.exports}}(this,function(e,t){"use strict";function n(e){function t(){var t=window.getComputedStyle(e,null);"vertical"===t.resize?e.style.resize="none":"both"===t.resize&&(e.style.resize="horizontal"),s="content-box"===t.boxSizing?-(parseFloat(t.paddingTop)+parseFloat(t.paddingBottom)):parseFloat(t.borderTopWidth)+parseFloat(t.borderBottomWidth),isNaN(s)&&(s=0),l()}function n(t){var n=e.style.width;e.style.width="0px",e.offsetWidth,e.style.width=n,e.style.overflowY=t}function o(e){for(var t=[];e&&e.parentNode&&e.parentNode instanceof Element;)e.parentNode.scrollTop&&t.push({node:e.parentNode,scrollTop:e.parentNode.scrollTop}),e=e.parentNode;return t}function r(){var t=e.style.height,n=o(e),r=document.documentElement&&document.documentElement.scrollTop;e.style.height="auto";var i=e.scrollHeight+s;return 0===e.scrollHeight?void(e.style.height=t):(e.style.height=i+"px",u=e.clientWidth,n.forEach(function(e){e.node.scrollTop=e.scrollTop}),void(r&&(document.documentElement.scrollTop=r)))}function l(){r();var t=Math.round(parseFloat(e.style.height)),o=window.getComputedStyle(e,null),i=Math.round(parseFloat(o.height));if(i!==t?"visible"!==o.overflowY&&(n("visible"),r(),i=Math.round(parseFloat(window.getComputedStyle(e,null).height))):"hidden"!==o.overflowY&&(n("hidden"),r(),i=Math.round(parseFloat(window.getComputedStyle(e,null).height))),a!==i){a=i;var l=d("autosize:resized");try{e.dispatchEvent(l)}catch(e){}}}if(e&&e.nodeName&&"TEXTAREA"===e.nodeName&&!i.has(e)){var s=null,u=e.clientWidth,a=null,p=function(){e.clientWidth!==u&&l()},c=function(t){window.removeEventListener("resize",p,!1),e.removeEventListener("input",l,!1),e.removeEventListener("keyup",l,!1),e.removeEventListener("autosize:destroy",c,!1),e.removeEventListener("autosize:update",l,!1),Object.keys(t).forEach(function(n){e.style[n]=t[n]}),i.delete(e)}.bind(e,{height:e.style.height,resize:e.style.resize,overflowY:e.style.overflowY,overflowX:e.style.overflowX,wordWrap:e.style.wordWrap});e.addEventListener("autosize:destroy",c,!1),"onpropertychange"in e&&"oninput"in e&&e.addEventListener("keyup",l,!1),window.addEventListener("resize",p,!1),e.addEventListener("input",l,!1),e.addEventListener("autosize:update",l,!1),e.style.overflowX="hidden",e.style.wordWrap="break-word",i.set(e,{destroy:c,update:l}),t()}}function o(e){var t=i.get(e);t&&t.destroy()}function r(e){var t=i.get(e);t&&t.update()}var i="function"==typeof Map?new Map:function(){var e=[],t=[];return{has:function(t){return e.indexOf(t)>-1},get:function(n){return t[e.indexOf(n)]},set:function(n,o){e.indexOf(n)===-1&&(e.push(n),t.push(o))},delete:function(n){var o=e.indexOf(n);o>-1&&(e.splice(o,1),t.splice(o,1))}}}(),d=function(e){return new Event(e,{bubbles:!0})};try{new Event("test")}catch(e){d=function(e){var t=document.createEvent("Event");return t.initEvent(e,!0,!1),t}}var l=null;"undefined"==typeof window||"function"!=typeof window.getComputedStyle?(l=function(e){return e},l.destroy=function(e){return e},l.update=function(e){return e}):(l=function(e,t){return e&&Array.prototype.forEach.call(e.length?e:[e],function(e){return n(e,t)}),e},l.destroy=function(e){return e&&Array.prototype.forEach.call(e.length?e:[e],o),e},l.update=function(e){return e&&Array.prototype.forEach.call(e.length?e:[e],r),e}),t.exports=l});
+!function (e, t) { if ("function" == typeof define && define.amd) define(["exports", "module"], t); else if ("undefined" != typeof exports && "undefined" != typeof module) t(exports, module); else { var n = { exports: {} }; t(n.exports, n), e.autosize = n.exports } }(this, function (e, t) { "use strict"; function n(e) { function t() { var t = window.getComputedStyle(e, null); "vertical" === t.resize ? e.style.resize = "none" : "both" === t.resize && (e.style.resize = "horizontal"), s = "content-box" === t.boxSizing ? -(parseFloat(t.paddingTop) + parseFloat(t.paddingBottom)) : parseFloat(t.borderTopWidth) + parseFloat(t.borderBottomWidth), isNaN(s) && (s = 0), l() } function n(t) { var n = e.style.width; e.style.width = "0px", e.offsetWidth, e.style.width = n, e.style.overflowY = t } function o(e) { for (var t = []; e && e.parentNode && e.parentNode instanceof Element;)e.parentNode.scrollTop && t.push({ node: e.parentNode, scrollTop: e.parentNode.scrollTop }), e = e.parentNode; return t } function r() { var t = e.style.height, n = o(e), r = document.documentElement && document.documentElement.scrollTop; e.style.height = "auto"; var i = e.scrollHeight + s; return 0 === e.scrollHeight ? void (e.style.height = t) : (e.style.height = i + "px", u = e.clientWidth, n.forEach(function (e) { e.node.scrollTop = e.scrollTop }), void (r && (document.documentElement.scrollTop = r))) } function l() { r(); var t = Math.round(parseFloat(e.style.height)), o = window.getComputedStyle(e, null), i = Math.round(parseFloat(o.height)); if (i !== t ? "visible" !== o.overflowY && (n("visible"), r(), i = Math.round(parseFloat(window.getComputedStyle(e, null).height))) : "hidden" !== o.overflowY && (n("hidden"), r(), i = Math.round(parseFloat(window.getComputedStyle(e, null).height))), a !== i) { a = i; var l = d("autosize:resized"); try { e.dispatchEvent(l) } catch (e) { } } } if (e && e.nodeName && "TEXTAREA" === e.nodeName && !i.has(e)) { var s = null, u = e.clientWidth, a = null, p = function () { e.clientWidth !== u && l() }, c = function (t) { window.removeEventListener("resize", p, !1), e.removeEventListener("input", l, !1), e.removeEventListener("keyup", l, !1), e.removeEventListener("autosize:destroy", c, !1), e.removeEventListener("autosize:update", l, !1), Object.keys(t).forEach(function (n) { e.style[n] = t[n] }), i.delete(e) }.bind(e, { height: e.style.height, resize: e.style.resize, overflowY: e.style.overflowY, overflowX: e.style.overflowX, wordWrap: e.style.wordWrap }); e.addEventListener("autosize:destroy", c, !1), "onpropertychange" in e && "oninput" in e && e.addEventListener("keyup", l, !1), window.addEventListener("resize", p, !1), e.addEventListener("input", l, !1), e.addEventListener("autosize:update", l, !1), e.style.overflowX = "hidden", e.style.wordWrap = "break-word", i.set(e, { destroy: c, update: l }), t() } } function o(e) { var t = i.get(e); t && t.destroy() } function r(e) { var t = i.get(e); t && t.update() } var i = "function" == typeof Map ? new Map : function () { var e = [], t = []; return { has: function (t) { return e.indexOf(t) > -1 }, get: function (n) { return t[e.indexOf(n)] }, set: function (n, o) { e.indexOf(n) === -1 && (e.push(n), t.push(o)) }, delete: function (n) { var o = e.indexOf(n); o > -1 && (e.splice(o, 1), t.splice(o, 1)) } } }(), d = function (e) { return new Event(e, { bubbles: !0 }) }; try { new Event("test") } catch (e) { d = function (e) { var t = document.createEvent("Event"); return t.initEvent(e, !0, !1), t } } var l = null; "undefined" == typeof window || "function" != typeof window.getComputedStyle ? (l = function (e) { return e }, l.destroy = function (e) { return e }, l.update = function (e) { return e }) : (l = function (e, t) { return e && Array.prototype.forEach.call(e.length ? e : [e], function (e) { return n(e, t) }), e }, l.destroy = function (e) { return e && Array.prototype.forEach.call(e.length ? e : [e], o), e }, l.update = function (e) { return e && Array.prototype.forEach.call(e.length ? e : [e], r), e }), t.exports = l });
 
 /* Ripple */
 
 function initRipple() {
   if (!document.querySelectorAll) return;
   var rippleHandlers = document.querySelectorAll('.ripple-handler');
-  var redraw = function(el) { el.offsetTop + 1; }
+  var redraw = function (el) { el.offsetTop + 1; }
   var isTouch = ('ontouchstart' in window);
   for (var i = 0; i < rippleHandlers.length; i++) {
-    (function(rippleHandler) {
+    (function (rippleHandler) {
       function onRippleStart(e) {
         var rippleMask = rippleHandler.querySelector('.ripple-mask');
         if (!rippleMask) return;
