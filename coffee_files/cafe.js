6d1b87af8b55809bfe6a1af99b29cf5a9bab0d7f
Bobby Ferdi <vertlink@gmail.com>
2023-05-17 16:44:33 +0700

    Update cafe.js

--- coffee_files/cafe.js
+++ coffee_files/cafe.js
@@ -123,7 +123,7 @@ var Cafe = {
     Cafe.updateItem(itemEl, delta);
   },
   formatPrice: function (price) {
-    return '$' + Cafe.formatNumber(price / 1000, 2, '.', ',');
+    return 'Rp' + Cafe.formatNumber(price, 2, ',', '.');
   },
   formatNumber: function (number, decimals, decPoint, thousandsSep) {
     number = (number + '').replace(/[^0-9+\-Ee.]/g, '')
